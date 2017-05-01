6385b3cd2d81f42d2ef31ead999cae7cbcc87c2e
droptheplot <novikov359@gmail.com>
2017-05-01 23:50:31 +0200

    Add graft function to node struct

--- src/node.go
+++ src/node.go
@@ -1,8 +1,44 @@
 package glug
 
+import (
+	"strings"
+)
+
 type node struct {
 	path     string
 	isParam  bool
 	methods  map[string][]Plug
 	children map[string]*node
+}
+
+func (curr *node) graft(method string, path string, plugs []Plug) {
+	parts := strings.Split(path, "/")[1:]
+	depth := len(parts)
+	prev := curr
+
+	for index, part := range parts {
+		if child, ok := prev.children[part]; ok {
+			curr = child
+		} else {
+			isParam := false
+
+			if part[0] == ':' {
+				isParam = true
+			}
+
+			curr = &node{
+				path:     part,
+				children: make(map[string]*node),
+				isParam:  isParam,
+			}
+		}
+
+		if depth == index+1 {
+			curr.methods = make(map[string][]Plug)
+			curr.methods[method] = plugs
+		}
+
+		prev.children[part] = curr
+		prev = curr
+	}
 }
